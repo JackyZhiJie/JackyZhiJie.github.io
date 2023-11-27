96714faf8cfd654538be9551fc849dfb5bcc86c0
Jacky <cameraoreo@gmail.com>
2023-11-27 17:31:16 +0800

    fix: bug

--- script.js
+++ script.js
@@ -4,12 +4,70 @@
   using graphics purchased from vectorstock.com
 */
 
+function handleTouchStart(event) {
+  event.preventDefault();
+  var touch = event.changedTouches[0];
+  var touchX = touch.pageX - canvas.offsetLeft;
+  var touchY = touch.pageY - canvas.offsetTop;
+  // Check if touch position is within player's bounds
+  if (
+    touchX >= player.x &&
+    touchX <= player.x + playerImage.width &&
+    touchY >= player.y &&
+    touchY <= player.y + playerImage.height
+  ) {
+    player.touchStartX = touchX;
+    player.touchStartY = touchY;
+  }
+}
+
+function handleTouchMove(event) {
+  event.preventDefault();
+  var touch = event.changedTouches[0];
+  var touchX = touch.pageX - canvas.offsetLeft;
+  var touchY = touch.pageY - canvas.offsetTop;
+  // Calculate the distance moved on the X and Y axes
+  var deltaX = touchX - player.touchStartX;
+  var deltaY = touchY - player.touchStartY;
+  // Move the player according to the distance
+  player.x += deltaX;
+  player.y += deltaY;
+
+  if (player.x < 0) {
+    player.x = 0;
+  } else if (player.x > canvas.width / 1.1) {
+    player.x = canvas.width / 1.1;
+  }
+
+  if (player.y < 0) {
+    player.y = 0;
+  } else if (player.y > canvas.height / 1.1) {
+    player.y = canvas.height / 1.1;
+  }
+
+  // Update touch start position for the next move
+  player.touchStartX = touchX;
+  player.touchStartY = touchY;
+}
+
+function handleTouchEnd(event) {
+  event.preventDefault();
+  // Reset touch start position
+  player.touchStartX = null;
+  player.touchStartY = null;
+}
+
 // Create the canvas for the game to display in
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 document.body.appendChild(canvas);
+
+// Touch event handlers
+canvas.addEventListener("touchstart", handleTouchStart, { passive: false });
+canvas.addEventListener("touchmove", handleTouchMove, { passive: false });
+canvas.addEventListener("touchend", handleTouchEnd, { passive: false });
 
 // Load the background image
 var bgReady = false;
@@ -25,21 +83,6 @@ bgImage.src =
 var playerReady = false;
 var playerImage = new Image();
 playerImage.onload = function () {
-  // show the player image
-  // Resize the enemy image to approximately 30 pixels
-  // var resizedWidth = 100;
-  // var aspectRatio = playerImage.width / playerImage.height;
-  // var resizedHeight = Math.round(resizedWidth / aspectRatio);
-
-  // var canvas = document.createElement("canvas");
-  // var ctx = canvas.getContext("2d");
-  // canvas.width = resizedWidth;
-  // canvas.height = resizedHeight;
-  // ctx.drawImage(playerImage, 0, 0, resizedWidth, resizedHeight);
-
-  // // Update the enemyImage variable with the resized image
-  // playerImage = new Image();
-  // playerImage.src = canvas.toDataURL();
   playerReady = true;
 };
 playerImage.src = "characterremovebackground.png";
@@ -49,21 +92,6 @@ playerImage.src = "characterremovebackground.png";
 var enemyReady = false;
 var enemyImage = new Image();
 enemyImage.onload = function () {
-  // Resize the enemy image to approximately 30 pixels
-  // var resizedWidth = 30;
-  // var aspectRatio = enemyImage.width / enemyImage.height;
-  // var resizedHeight = Math.round(resizedWidth / aspectRatio);
-
-  // var canvas = document.createElement("canvas");
-  // var ctx = canvas.getContext("2d");
-  // canvas.width = resizedWidth;
-  // canvas.height = resizedHeight;
-  // ctx.drawImage(enemyImage, 0, 0, resizedWidth, resizedHeight);
-
-  // // Update the enemyImage variable with the resized image
-  // enemyImage = new Image();
-  // enemyImage.src = canvas.toDataURL();
-
   // Show the enemy image
   enemyReady = true;
 };
@@ -73,29 +101,13 @@ enemyImage.src = "huawei-mate-60-rs.jpg";
 // Create the game objects
 var player = {
   speed: 256, // movement speed of player in pixels per second
+  x: 0,
+  y: 0,
+  touchStartX: canvas.width / 2,
+  touchStartY: canvas.height / 2,
 };
 var enemy = {};
 var enemiesCaught = 0;
-
-// Handle keyboard controls
-var keysDown = {};
-
-// Check for keys pressed where key represents the key pressed
-addEventListener(
-  "keydown",
-  function (event) {
-    keysDown[event.key] = true;
-  },
-  false
-);
-
-addEventListener(
-  "keyup",
-  function (event) {
-    delete keysDown[event.key];
-  },
-  false
-);
 
 // Reset the player and enemy positions when player catches an enemy
 var reset = function () {
@@ -110,96 +122,7 @@ var reset = function () {
     enemyImage.height + Math.random() * (canvas.height - enemyImage.height * 2);
 };
 
-// Update game objects - change player position based on key pressed
-// var update = function (modifier) {
-//   if ("ArrowUp" in keysDown) {
-//     // Player is holding up key
-//     player.y -= player.speed * modifier;
-//   }
-//   if ("ArrowDown" in keysDown) {
-//     // Player is holding down key
-//     player.y += player.speed * modifier;
-//   }
-//   if ("ArrowLeft" in keysDown) {
-//     // Player is holding left key
-//     player.x -= player.speed * modifier;
-//   }
-//   if ("ArrowRight" in keysDown) {
-//     // Player is holding right key
-//     player.x += player.speed * modifier;
-//   }
-
-//   // Check if player and enemy collide
-//   if (
-//     player.x <= enemy.x + enemyImage.width &&
-//     enemy.x <= player.x + playerImage.width &&
-//     player.y <= enemy.y + enemyImage.height &&
-//     enemy.y <= player.y + playerImage.height
-//   ) {
-//     ++enemiesCaught;
-//     reset();
-//   }
-// };
-
-var update = function (modifier) {
-  // Touch event handlers
-  canvas.addEventListener("touchstart", handleTouchStart, false);
-  canvas.addEventListener("touchmove", handleTouchMove, false);
-  canvas.addEventListener("touchend", handleTouchEnd, false);
-
-  function handleTouchStart(event) {
-    event.preventDefault();
-    var touch = event.changedTouches[0];
-    var touchX = touch.pageX - canvas.offsetLeft;
-    var touchY = touch.pageY - canvas.offsetTop;
-    // Check if touch position is within player's bounds
-    if (
-      touchX >= player.x &&
-      touchX <= player.x + playerImage.width &&
-      touchY >= player.y &&
-      touchY <= player.y + playerImage.height
-    ) {
-      player.touchStartX = touchX;
-      player.touchStartY = touchY;
-    }
-  }
-
-  function handleTouchMove(event) {
-    event.preventDefault();
-    var touch = event.changedTouches[0];
-    var touchX = touch.pageX - canvas.offsetLeft;
-    var touchY = touch.pageY - canvas.offsetTop;
-    // Calculate the distance moved on the X and Y axes
-    var deltaX = touchX - player.touchStartX;
-    var deltaY = touchY - player.touchStartY;
-    // Move the player according to the distance
-    player.x += deltaX;
-    player.y += deltaY;
-
-    if (player.x < 0) {
-      player.x = 0;
-    } else if (player.x > canvas.width / 1.1) {
-      player.x = canvas.width / 1.1;
-    }
-
-    if (player.y < 0) {
-      player.y = 0;
-    } else if (player.y > canvas.height / 1.1) {
-      player.y = canvas.height / 1.1;
-    }
-
-    // Update touch start position for the next move
-    player.touchStartX = touchX;
-    player.touchStartY = touchY;
-  }
-
-  function handleTouchEnd(event) {
-    event.preventDefault();
-    // Reset touch start position
-    player.touchStartX = null;
-    player.touchStartY = null;
-  }
-
+var update = function () {
   // Check if player and enemy collide
   if (
     player.x <= enemy.x + enemyImage.width &&
@@ -262,7 +185,7 @@ setInterval(counter, 1000);
 // The main game loop
 var main = function () {
   // run the update function
-  update(0.02); // do not change
+  update(); // do not change
   // run the render function
   render();
   // Request to do this again ASAP
